cd75e827281898b3dadbe4d0a2b36761d947fccd
annaelizabeth2019 <annapeterson89@gmail.com>
2019-05-09 17:15:04 -0500

    finished doing a scrape of one page

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ uClient.close()
 page_soup = soup(webpage, 'html.parser')
 info = page_soup.find("div", {"id": "whiskyDetailsWrapper", "class" : "expanderBox boxBgr"})
 
-name = info.h2
-country = info.a
+file_name = "webscrape.csv"
+f = open(file_name, "w")
+
+headers = "name, country, brand, bottler, style, alcohol, volume\n"
+
+name = page_soup.h1.text
+country = info.find("div", {"id":"ContentPlaceHolder1_ctl00_ctl00_wdCountry"}).find("span", {"class": "kv-val"}).text
 brand = info.find(itemprop="brand").text
 bottler = info.find(itemprop="manufacturer").text
-style = info.find(id="ContentPlaceHolder1_ctl00_ctl00_wdStyle").a.text
+style = info.find(id="ContentPlaceHolder1_ctl00_ctl00_wdStyle").find("span", {"class": "kv-val"}).text
 alcohol = info.find("div", {"id":"ContentPlaceHolder1_ctl00_ctl00_wdAlcohol"}).find("span", {"class": "kv-val"}).text
-alcohol = info.find("div", {"id":"ContentPlaceHolder1_ctl00_ctl00_wdVolume"}).find("span", {"class": "kv-val"}).text
+volume = info.find("div", {"id":"ContentPlaceHolder1_ctl00_ctl00_wdVolume"}).find("span", {"class": "kv-val"}).text
 
+f.write(headers)
+
+f.write(name + "," + country + "," + brand + "," + bottler + "," + style + "," + alcohol + "," + volume + "\n")
+
+f.close()
